588da444a822b7fdab199ee2fc17cb07cd486cf4
miaumiau <dev@xcash.rocks>
2020-10-06 15:44:39 +0200

    sidemenu changed icons blocks found and voters list

--- src/app/core/sidemenu/menu-element.ts
+++ src/app/core/sidemenu/menu-element.ts
@@ -9,13 +9,13 @@ export const menus = [
         'name': 'Blocks Found',
         'open': false,
         'link': 'tables/blocksfound',
-        'icon': 'assignments',
+        'icon': 'find_in_page',
     },
     {
         'name': 'Voters List',
         'open': false,
         'link': 'tables/voterslist',
-        'icon': 'edit',
+        'icon': 'grading',
     },
     {
         'name': 'API',
