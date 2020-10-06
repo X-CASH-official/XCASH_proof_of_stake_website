7ceee8dc38637f5a0e1a321503091cbdd7bfb666
miaumiau <dev@xcash.rocks>
2020-10-06 19:47:56 +0200

    rearange table voters list, table style fixes

--- src/app/tables/voterslist/voterslist.component.ts
+++ src/app/tables/voterslist/voterslist.component.ts
@@ -19,51 +19,47 @@ export class voterslistComponent implements OnInit {
 	public_address:string;
 	data:any[] = [];
 	document_start_count:number = 1;
-	public displayedColumns = ['ID', 'public_address_created_reserve_proof', 'total', 'reserve_proof'];
+
+  public displayedColumns = ['ID', 'total', 'public_address_created_reserve_proof', 'reserve_proof'];
 	public exampleDatabase;
 	public dataSource: ExampleDataSource | null;
-  	public showFilterTableCode;
-  	constructor(private httpdataservice: httpdataservice) { }
+	public showFilterTableCode;
 
-  	ngOnInit() {
+	constructor(private httpdataservice: httpdataservice) { }
 
-          // get the data
-	  this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_STATISTICS).subscribe(
-	  (res) =>
-	  {
+	ngOnInit() {
+    // get the data
+    this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_STATISTICS).subscribe(
+      (res) =>   {
+        var data = JSON.parse(JSON.stringify(res));
+        this.public_address = data.public_address;
+
+        // get the data
+        this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_VOTERS_LIST + "?parameter1=" + this.public_address).subscribe(
+          (res) => {
+            this.exampleDatabase = new ExampleDatabase();
             var data = JSON.parse(JSON.stringify(res));
-            this.public_address = data.public_address;
-
-            // get the data
-	    this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_VOTERS_LIST + "?parameter1=" + this.public_address).subscribe(
-	    (res) =>
-	    {
-              this.exampleDatabase = new ExampleDatabase();
-              var data = JSON.parse(JSON.stringify(res));
-	      this.total_vote_count = 0;
-	      this.amount_of_votes = data.length;
-	      var count = 0;
-              var total = 0;
-              for (count = 0; count < this.amount_of_votes; count++)
-	      {
-                total = parseInt(data[count].total) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT;
-                this.total_vote_count += total;
-	        this.exampleDatabase.addUser((count + 1).toString(),data[count].public_address_created_reserve_proof.toString(),total.toString(),data[count].reserve_proof.toString());
-	      }
-	      this.dashCard[0].number = this.total_vote_count;
-	      this.dashCard[1].number = this.amount_of_votes;
-  	      this.dataSource = new ExampleDataSource(this.exampleDatabase);
-              },
-              (error) =>
-              {
-                Swal.fire("Error","An error has occured","error");
-              }
-	    );
-          },
-            (error) =>
-            {
-              Swal.fire("Error","An error has occured","error");
+            this.total_vote_count = 0;
+            this.amount_of_votes = data.length;
+            var count = 0;
+            var total = 0;
+            for (count = 0; count < this.amount_of_votes; count++) {
+              total = parseInt(data[count].total) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT;
+              this.total_vote_count += total;
+              this.exampleDatabase.addUser((count + 1).toString(),data[count].public_address_created_reserve_proof.toString(),total.toString(),data[count].reserve_proof.toString());
             }
-	  );
-	}
+            this.dashCard[0].number = this.total_vote_count;
+            this.dashCard[1].number = this.amount_of_votes;
+            this.dataSource = new ExampleDataSource(this.exampleDatabase);
+          },
+          (error) => {
+            Swal.fire("Error","An error has occured","error");
+          }
+        );
+      },
+      (error) => {
+        Swal.fire("Error","An error has occured","error");
+      }
+    );
+  }
 }
