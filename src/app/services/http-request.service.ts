900a8d5d274c19437a583708f0e5625a4df4eb5e
miaumiau <dev@xcash.rocks>
2020-12-20 15:45:31 +0100

    refactor:  added baseURL, API-URLs, default-theme to environment configuration files and added environment API-URLs in http-request

--- src/app/services/http-request.service.ts
+++ src/app/services/http-request.service.ts
@@ -1,23 +1,21 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
+import { environment } from './../../environments/environment';
+
 @Injectable()
 export class HttpdataService{
   constructor(private httpClient: HttpClient) {}
 
-  // specify the delegate node data source (empty = localhost) e.g.: http://node.xcash.rocks
-  node_server_port:string = ""; //
-  // specify the delegates explorer data source
-  explorer_server_port:string = "http://delegates.xcash.foundation";
 
-  POOL_GET_STATISTICS:string = this.node_server_port + "/shareddelegateswebsitegetstatistics";
-  POOL_GET_BLOCKS_FOUND:string = this.node_server_port + "/getblocksfound";
-  POOL_GET_PUBLIC_ADDRESS_INFORMATION:string = this.node_server_port + "/getpublicaddressinformation";
-  POOL_GET_DELEGATES_VOTERS_LIST:string = this.node_server_port + "/getdelegatesvoterslist";
-  POOL_GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION:string = this.node_server_port + "/getpublicaddresspaymentinformation";
+  POOL_GET_STATISTICS:string = environment.node_apiEndPoint + "/shareddelegateswebsitegetstatistics";
+  POOL_GET_BLOCKS_FOUND:string = environment.node_apiEndPoint + "/getblocksfound";
+  POOL_GET_PUBLIC_ADDRESS_INFORMATION:string = environment.node_apiEndPoint + "/getpublicaddressinformation";
+  POOL_GET_DELEGATES_VOTERS_LIST:string = environment.node_apiEndPoint + "/getdelegatesvoterslist";
+  POOL_GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION:string = environment.node_apiEndPoint + "/getpublicaddresspaymentinformation";
 
-  EXPLORER_GET_DELEGATES_STATISTICS:string =  this.explorer_server_port + "/getdelegatesstatistics";
-  EXPLORER_GET_DELEGATE_WEBSITE_STATISTICS:string =  this.explorer_server_port + "/delegateswebsitegetstatistics";
+  EXPLORER_GET_DELEGATES_STATISTICS:string =  environment.explorer_apiEndPoint + "/getdelegatesstatistics";
+  EXPLORER_GET_DELEGATE_WEBSITE_STATISTICS:string =  environment.explorer_apiEndPoint + "/delegateswebsitegetstatistics";
 
   XCASH_WALLET_DECIMAL_PLACES_AMOUNT:number = 1000000;
   BLOCK_TIME:number = 5;
