4b75fd401435856fb8d243811a835f90c6b3d1ef
miaumiau <dev@xcash.rocks>
2020-12-23 03:26:49 +0100

    fix: Remove this unused import of 'OnInit'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavigationStart, NavigationEnd, Router } from '@angular/router';
 import { Meta } from '@angular/platform-browser';
 
